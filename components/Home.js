70292c7cd4e3cbe2be546e305b22e2c0195d6378
OldFresco <hinsonkojo@gmail.com>
2016-11-29 02:25:41 +0000

    Tweak home page styling

--- components/Home.js
+++ components/Home.js
@@ -8,11 +8,11 @@ class Home extends Component {
       <article>
         <div className='container-fluid'>
           <div className='main-display col-md-8'>
-              <h1>Kiwwi</h1>
+              <h1><font color='#1B3628'>Kiwwi*</font></h1>
               <h1 className='plan-style'><font color='antiquewhite'>PLAN</font> WELL.</h1>
-              <h1>EAT WELL.</h1>
-              <h1>STAY ACTIVE.</h1>
-              <h1>Repeat.</h1>
+              <h1 className='plan-style'><font color='antiquewhite'>EAT</font> WELL.</h1>
+              <h1>STAY <font color='antiquewhite'>ACTIVE</font>.</h1>
+              <h1 className='plan-style'>Repeat.</h1>
           </div>
           <div className='col-md-2 main-logo'>
             <img src={logoUrl} />
