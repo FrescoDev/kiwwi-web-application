cfca5f2da1d9739678e75b454869afe5f226d567
OldFresco <hinsonkojo@gmail.com>
2016-11-30 23:30:07 +0000

    Add tabs to dashboard

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React from 'react';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import FontIcon from 'material-ui/FontIcon';
+import ActionFlightTakeoff from 'material-ui/svg-icons/action/flight-takeoff';
 
-function Dashboard () {
-  return (
-    <article>
-      <section className='text-section'>
-        <h1>Dashboard</h1>
-        <p>
-          Welcome, you are logged in! To have a look at the code behind this application, go to <a href='https://github.com/sotojuan/saga-login-flow'>Github</a>.
-        </p>
-      </section>
-    </article>
-  )
-}
-
+const Dashboard = () => (
+  <article>
+        <div className='container-fluid'>
+          <div className='main-display col-md-8'>
+              <h1><font color='#1B3628'>Welcome</font></h1>
+              <h1 className='plan-style'><font color='antiquewhite'>To</font> Your</h1>
+              <h1 className='plan-style'><font color='antiquewhite'>Dashboard</font></h1>
+              <h1 className='plan-style'>:)</h1>
+          </div>
+       </div>        
+  <Tabs className='tabs'>
+    <Tab style={{
+          backgroundColor: '#2BC677'
+        }} 
+        icon={<ActionFlightTakeoff />} />
+    <Tab style={{
+          backgroundColor: '#2BC677'
+        }}
+        icon={<ActionFlightTakeoff />} />
+    <Tab style={{
+          backgroundColor: '#2BC677'
+        }}
+        icon={<ActionFlightTakeoff />} />
+    <Tab style={{
+          backgroundColor: '#2BC677'
+        }}
+        icon={<ActionFlightTakeoff />} />
+  </Tabs>
+  </article>
+);
 export default Dashboard
